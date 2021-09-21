504b56d1381f64f7237957b8fa29c4baa4223e76
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-22 07:52:06 +0900

    インデント修正

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -26,16 +26,16 @@
     <form action="/articles/{{ $article->id }}" method="post">
         @csrf
         @method('PATCH')
-            <p>
-                <label for="title">論文タイトル</label>
-                <input type="text" name="title" value="{{ old('title', $article->title) }}">
-            </p>
-            <p>
-                <label for="body">本文</label>
-                <textarea name="body" id="body">{{ old('body', $article->body) }}</textarea>
-            </p>
-            <input type="submit" value="更新">
-        </form>
+        <p>
+            <label for="title">論文タイトル</label>
+            <input type="text" name="title" value="{{ old('title', $article->title) }}">
+        </p>
+        <p>
+            <label for="body">本文</label>
+            <textarea name="body" id="body">{{ old('body', $article->body) }}</textarea>
+        </p>
+        <input type="submit" value="更新">
+    </form>
 </body>
 
 </html>
