e4c32702430723d85dacd041214aea5191156e47
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-21 09:37:48 +0900

    前後修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,15 +7,15 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index', ['articles' => $articles]);
+    }
+
     public function show($id)
     {
         $article = Article::find($id);
         return view('articles.show', ['article' => $article]);
-    }
-
-    public function index()
-    {
-        $articles = Article::all();
-        return view('articles.index',['articles' => $articles]);
     }
 }
